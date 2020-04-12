005e372b14c6701318bd1060ecb11a16d7464890
ryoga <surpace2479@gmail.com>
2020-04-12 18:25:24 +0900

    update: 最速

--- trajectory_generator_gui.py
+++ trajectory_generator_gui.py
@@ -51,23 +51,23 @@ class TrajectoryGeneratorGui(QMainWindow, Ui_MainWindow):
             return
 
         # 書き込むテキストの生成
-        # write_text = ""
-        # for i in range(len(self.pm.trajectory)):
-        #     write_text += str(self.pm.trajectory[i][0])
-        #     write_text += ","
-        #     write_text += str(self.pm.trajectory[i][1])
-        #     write_text += "\n"
-
-        write_text = "["
+        write_text = ""
         for i in range(len(self.pm.trajectory)):
-            write_text += "["
             write_text += str(self.pm.trajectory[i][0])
             write_text += ","
             write_text += str(self.pm.trajectory[i][1])
-            write_text += "]"
-            if i != len(self.pm.trajectory) - 1:
-                write_text += ","
-        write_text += "]"
+            write_text += "\n"
+
+        # write_text = "["
+        # for i in range(len(self.pm.trajectory)):
+        #     write_text += "["
+        #     write_text += str(self.pm.trajectory[i][0])
+        #     write_text += ","
+        #     write_text += str(self.pm.trajectory[i][1])
+        #     write_text += "]"
+        #     if i != len(self.pm.trajectory) - 1:
+        #         write_text += ","
+        # write_text += "]"
 
         # 書き込み
         self.saveFile(fname, write_text)
